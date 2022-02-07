2aea5d6a356f52e9f47b5705c2b3a0d012d9a31c
“jamesquinn00git config --global user.email “jamesquinn00@outlook.com <jamesquinn00@outlook.comgit config --global user.name jamesquinn00>
2022-02-07 20:18:21 +0000

    Url link working

--- client/src/components/FlightForm.js
+++ client/src/components/FlightForm.js
@@ -2,27 +2,68 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
 const FlightForm = () => {
+    
+    const [origins, setOrigins] = useState([])
+    const [destinations, setDestinations] = useState([])
+    const [adults, setAdults] = useState()
+    const [children, setChildren] = useState()
+    const [outDate, setOutDate] = useState()
+    const [returnDate, setReturnDate] = useState()
+    const [destinationCode, setDestinationCode]= useState()
+    const [originCode, setOriginCode]= useState()
 
-    const destination = "Paris"
-    
-    // "https://www.skyscanner.net/transport/flights/lond/nyca/220209/220216/"
-    // "?adults=1&adultsv2=1&cabinclass=economy&children=0&childrenv2="
-    // "&inboundaltsenabled=false&infants=0&outboundaltsenabled=false"
-    // "&preferdirects=false&ref=home&rtn=1"
-    
-    async function makeUrl(e){
+    const [url, setUrl]=useState()
+
+    async function getAirports(e){
         e.preventDefault()
-        const url = "https://www.skyscanner.net/transport/flights/"
-
-        const body = {'destination': 'Paris', 'origin': 'Los Angeles'}
-
-
+        const body = {'destination': 'Los Angeles', 'origin': e.target[0].value }
         const results = await axios.post("http://localhost:8000/airports/", body)
-        console.log(results)
+        const potential_destinations = []
+        const potential_origins = []
+        for(let x in results.data.origin){
+            potential_origins.push(results.data.origin[x])
+        }
+        setOrigins(potential_origins)
+
+        for(let x in results.data.destination){
+            potential_destinations.push(results.data.destination[x])
+        }
+        setDestinations(potential_destinations)
+        setDestinationCode(potential_destinations[0].code)
+
+        setAdults(e.target[3].value)
+        setChildren(e.target[4].value)
+
+        //format date correctly ready for URL
+        let formattedOutDate = e.target[1].value.substring(2).replace(/-/g,'')
+        let formattedReturnDate = e.target[2].value.substring(2).replace(/-/g,'')
+        setOutDate(formattedOutDate)
+        setReturnDate(formattedReturnDate)
+        
+    }
+
+    function generateLink(e){
+        // "https://www.skyscanner.net/transport/flights/lond/nyca/220209/220216/"
+        // "?adults=1&adultsv2=1&cabinclass=economy&children=0&childrenv2="
+        // "&inboundaltsenabled=false&infants=0&outboundaltsenabled=false"
+        // "&preferdirects=false&ref=home&rtn=1"
+        e.preventDefault()
+        if(e.target.name === "origin-list"){
+            setOriginCode(e.target.value)
+        }
+        else if(e.target.name === "destination-list"){
+            setDestinationCode(e.target.value)
+        }
+
+        if(originCode && destinationCode){
+            const url = `https://www.skyscanner.net/transport/flights/${originCode}/${destinationCode}/${outDate}/${returnDate}?adults=${adults}&adultsv2=1&cabinclass=economy&children=${children}&childrenv2=&inboundaltsenabled=false&infants=0&outboundaltsenabled=false&preferdirects=false&ref=home&rtn=1`
+            setUrl(url)
+        }
     }
 
     return(
-        <form id="flight-form" onSubmit={makeUrl}>
+        <>
+        <form id="flight-form" onSubmit={getAirports}>
             <input name="from" type="text"/>From: 
             <input name="outboundDate" type="date"/>Outbound flight:
             <input name="returnDate" type="date"/>Return:
@@ -30,6 +71,21 @@ const FlightForm = () => {
             <input name="children" type="number" min="0" max="8"/>Children:
             <input type="submit"/>Submit
         </form>
+        {origins.length > 0 ?   <form className="potential-airports">
+                                            <select name="origin-list" onChange={generateLink}>
+                                                {origins.map( x => <option value={x.code} key={x.code}>{x.name}</option>)}
+                                            </select>
+                                </form>
+        : <></>}
+        {destinations.length > 0 ? <form className="potential-airports">
+                                            <select name="destination-list" onChange={generateLink}>
+                                                {destinations.map( x => <option value={x.code} key={x.code}>{x.name}</option>)}
+                                            </select>
+                                        </form>
+        : <></>}
+        {/* {destinations.length == 0 ? <p>Currently no flights to this destination</p> : <> </>} */}
+        {url? <a href={url}>Click Here For Flights!</a> : <> </>}
+        </>
     )
 }
 
--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -87,42 +87,42 @@ const Options = ({getResults}) => {
         <form id="option-form" onSubmit={handleSubmit}>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="0" name="art"/>Art
-                <i class="fas fa-palette"></i>
+                <i className="fas fa-palette"></i>
                 {art? <>{(Math.round(artValue * 100) / 100).toFixed(1)} <input type="range" name="artValue" min="5" max="10" value={artValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="1" name="beaches"/>Beaches
-                <i class="fas fa-umbrella-beach"></i>
+                <i className="fas fa-umbrella-beach"></i>
                 {beaches? <>{(Math.round(beachesValue * 100) / 100).toFixed(1)} <input type="range" name="beachesValue" min="5" max="12" value={beachesValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="2" name="cuisine"/>Cuisine
-                <i class="fas fa-utensils"></i>
+                <i className="fas fa-utensils"></i>
                 {cuisine? <>{(Math.round(cuisineValue * 100) / 100).toFixed(1)} <input type="range" name="cuisineValue" min="5" max="10" value={cuisineValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="3" name="golf"/>Golf
-                <i class="fas fa-golf-ball"></i>
+                <i className="fas fa-golf-ball"></i>
                 {golf? <>{(Math.round(golfValue * 100) / 100).toFixed(1)} <input type="range" name="golfValue" min="5" max="10" value={golfValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="3" name="museums"/>Museums
-                <i class="fas fa-landmark"></i>
+                <i className="fas fa-landmark"></i>
                 {museums? <>{(Math.round(museumsValue * 100) / 100).toFixed(1)} <input type="range" name="museumsValue" min="5" max="10" value={museumsValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="3" name="skiing"/>Skiing
-                <i class="fas fa-skiing"></i>
+                <i className="fas fa-skiing"></i>
                 {skiing? <>{(Math.round(skiingValue * 100) / 100).toFixed(1)} <input type="range" name="skiingValue" min="2" max="4.6" value={skiingValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="3" name="hiking"/>Hiking
-                <i class="fas fa-hiking"></i>
+                <i className="fas fa-hiking"></i>
                 {hiking? <>{(Math.round(hikingValue * 100) / 100).toFixed(1)} <input type="range" name="hikingValue" min="5" max="13" value={hikingValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
             <div>
                 <input type="checkbox" onChange={handleCheck} value="3" name="nightlife"/>Nightlife
-                <i class="fas fa-cocktail"></i>
+                <i className="fas fa-cocktail"></i>
                 {nightlife? <>{(Math.round(nightlifeValue * 100) / 100).toFixed(1)} <input type="range" name="nightlifeValue" min="5" max="10" value={nightlifeValue} step="0.2" onChange={handleSlide}></input></> : <></>}
             </div>
                 {noneChecked? <input disabled type="submit"/>: <input type="submit"/>}
